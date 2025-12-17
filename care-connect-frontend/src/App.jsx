36f3b8eaf08c1c7c280acef694c019d26ebc8fb4
U L R Kavindya <it23600898@my.sliit.lk>
2025-12-17 05:44:20 +0530

    Initial Setup - Backend and Frontend

---
+++ care-connect-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Care Connect Frontend 🏥</h1>
+      <p>Running inside Docker!</p>
+    </div>
+  );
+}
+export default App;
